52c9ad5381513cac09a696aef0daaae5e76da752
ceilf6 <3506456886@qq.com>
2026-06-12 15:43:13 +0800

    Register wave15_exploration_baseline_closure_complete as frontier go state.

--- src/frontier_go_no_go_board.py
+++ src/frontier_go_no_go_board.py
@@ -71,6 +71,7 @@ def classify_go_no_go_state(current_state: str) -> str:
         "wave12_exploration_baseline_closure_complete",
         "wave13_exploration_baseline_closure_complete",
         "wave14_exploration_baseline_closure_complete",
+        "wave15_exploration_baseline_closure_complete",
         "speaker_profile_case_scope_coordination_complete",
         "speaker_profile_lightoverlap_diagnostic_coordination_complete",
         "speaker_profile_midoverlap_diagnostic_coordination_complete",
